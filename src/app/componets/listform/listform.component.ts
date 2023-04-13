0a973f5bf0020a42f7c488c60228d1a7516ff506
JIMMYHUNK <jagiraldo497@misena.edu.co>
2023-04-13 15:28:16 -0500

    Modificacion css a scss eliminacion de recap.ts

--- src/app/componets/listform/listform.component.ts
+++ src/app/componets/listform/listform.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-listform',
   templateUrl: './listform.component.html',
-  styleUrls: ['./listform.component.css']
+  styleUrls: ['./listform.component.scss']
 })
 export class ListformComponent {
 
